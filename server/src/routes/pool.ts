97422d285f1e4a59e9f62e85e3c21892b698983e
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-11-07 10:06:56 -0300

    funcionalidade de entrar em bolão

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -3,6 +3,7 @@ import { prisma } from "../lib/prisma"
 import { z } from "zod"
 import ShortUniqueId from  "short-unique-id"
 import jwt from "@fastify/jwt"
+import { authenticate } from "../plugins/authenticate"
 
 export async function poolRoutes(fastify: FastifyInstance){
 
@@ -63,6 +64,67 @@ export async function poolRoutes(fastify: FastifyInstance){
     return reply.status(201).send({ code })
 
    })
+
+   //entrar em um bolão (ja auntenticado)
+   fastify.post("/pools/:id/join", {
+    onRequest: [authenticate]
+   }, async (request, reply) => {
+
+    const joinPoolBody = z.object({
+        code: z.string(),
+    })
+
+    const { code } = joinPoolBody.parse(request.body)
+
+    const pool = await prisma.pool.findUnique({
+        where: {
+            code,
+        }, 
+        include: {
+            participants:{
+                where:{
+                    userId: request.user.sub,
+                }
+            }
+        }
+    })
+
+    if(!pool){
+        return reply.status(400).send({
+            message:"Poll not find."
+        })
+    }
+
+    if(pool.participants.length > 0){
+        return reply.status(400).send({
+            message:"You already joined this poll!."
+        })
+    }
+
+    //se o bolão ainda não tiver um dono, o primeiro usuario que entra no bolão se torna o dono
+    if(!pool.ownerId){
+        await prisma.pool.update({
+            where: {
+                id:pool.id,
+            }, 
+            data:{
+                ownerId: request.user.sub,
+            }
+        })
+    }
+
+    await prisma.participant.create({
+        data: {
+            poolId: pool.id,
+            userId: request.user.sub,
+        }
+    })
+
+    return reply.status(201).send()
+
+   })
+
+   
 
 }
 
